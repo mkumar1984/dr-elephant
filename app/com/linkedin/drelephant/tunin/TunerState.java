a3277a9284ed10d9b3c3151666005d1c16ac0900
Arpan Agrawal <aragrawal@linkedin.com>
2017-11-07 04:49:22 -0800

    Removing comments

--- app/com/linkedin/drelephant/tunin/TunerState.java
+++ app/com/linkedin/drelephant/tunin/TunerState.java
@@ -6,15 +6,9 @@ import models.Job;
 import java.util.List;
 
 public class TunerState {
+
     private Job _tuningJob;
-
-//    private List<Particle> _archive;
-//    private List<Particle> _previousPopulation;
-//    private List<List<Integer>> _randomState;
-//    private List<Particle> _currentPopulation;
     private String _stringTunerState;
-
-    // Todo: The following won't be required once, algo class contains the list of parameters
     private List<AlgoParam> _parametersToTune;
 
     public void setTuningJob(Job tuningJob){
@@ -32,30 +26,7 @@ public class TunerState {
     public String getStringTunerState(){
         return _stringTunerState;
     }
-//
-//    public void setPreviousPopulation(List<Particle> previousPopulation){
-//        this._previousPopulation = previousPopulation;
-//    }
-//
-//    public List<Particle> getPreviousPopulation(){
-//        return _previousPopulation;
-//    }
-//
-//    public void setCurrentPopulation(List<Particle> currentPopulation){
-//        this._currentPopulation = currentPopulation;
-//    }
-//
-//    public List<Particle> getCurrentPopulation(){
-//        return _currentPopulation;
-//    }
-//
-//    public void setRandomState(List<List<Integer>> randomState){
-//        this._randomState = randomState;
-//    }
-//
-//    public List<List<Integer>> getRandomState(){
-//        return _randomState;
-//    }
+
 
     public void setParametersToTune(List<AlgoParam> parameters){ this._parametersToTune = parameters; }
 
