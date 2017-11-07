de56bca0d421bb1e95633b07ec5c8394b1309eea
Arpan Agrawal <aragrawal@linkedin.com>
2017-11-07 02:08:50 -0800

    Method to get tuner state added

--- app/com/linkedin/drelephant/tunin/ParamGenerator.java
+++ app/com/linkedin/drelephant/tunin/ParamGenerator.java
@@ -2,11 +2,16 @@ package com.linkedin.drelephant.tunin;
 
 import com.avaje.ebean.Expr;
 import models.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import play.libs.Json;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class ParamGenerator {
+
+
+    abstract TunerState generateParamSet(TunerState jobTunerState);
 
     private List<Job> fetchJobsForParamSuggestion(){
         List<Job> jobsForSwarmSuggestion = new ArrayList<>();
@@ -16,18 +21,36 @@ public abstract class ParamGenerator {
                 .not(Expr.eq("paramSetState", JobExecution.ParamSetStatus.EXECUTED))
                 .select("jobId").setDistinct(true).findSet()){
             Job tuninJob = new Job();
-            tuninJob.jobId = paramSetMetaData.jobId;
+            tuninJob.jobId = paramSetMetaData.job.jobId;
             jobsForSwarmSuggestion.add(tuninJob);
         }
         return jobsForSwarmSuggestion;
     }
 
-    private List<TunerState> getJobsTunerState(List<Job> tuninJobs){
-        // can be done only after saved state model is created
-        // Todo: fitness of current pop will be populated where?
+    private List<Particle> jsonToParticleList(JsonNode jsonParticleList){
+
     }
 
-    abstract TunerState generateParamSet(TunerState jobTunerState);
+    private List<TunerState> getJobsTunerState(List<Job> tuninJobs){
+        // Todo: fitness of current pop will be populated where?
+        List<TunerState> tunerStateList = new ArrayList<>();
+        for (Job job: tuninJobs){
+            TunerState tunerState = new TunerState();
+            tunerState.setTuningJob(job);
+            JobSavedState jobSavedState = JobSavedState.find.byId(job.jobId);
+            String savedState = new String(jobSavedState.savedState);
+            // Need to add fitness to the current population
+            tunerState.setStringTunerState(savedState);
+            int algoId = job.algo.algoId;
+
+            List<AlgoParam> algoParamList = AlgoParam.find.where().Expr.eq()
+
+            tunerStateList.add(tunerState);
+        }
+        return tunerStateList;
+    }
+
+
 
 
     private void updateDatabase(List<TunerState> jobTunerStateList){
@@ -37,29 +60,48 @@ public abstract class ParamGenerator {
          * Update the param in the job execution table, hob suggestedparam table
          * update the tuner state
          */
-        saveTunerState(jobTunerStateList);
+
+        List<JobExecution> jobExecutionList = new ArrayList<>();
+        List<JobSuggestedParamValue> jobSuggestedParamValueList= new ArrayList<>();
 
         for (TunerState jobTunerState: jobTunerStateList){
 
             Job job = jobTunerState.getTuningJob();
-            int jobId = job.jobId;
             List<AlgoParam> paramList = jobTunerState.getParametersToTune();
-            List<Individual> suggestedPopulation = jobTunerState.getCurrentPopulation();
 
-            List<JobSuggestedParamValue> jobSuggestedParamValueList= new ArrayList<>();
+            String stringTunerState = jobTunerState.getStringTunerState();
+            JsonNode jsonTunerState = Json.toJson(stringTunerState);
 
-            for (Individual suggestedParticle: suggestedPopulation){
+            // Todo: From the json extract the currentPopulaion list of individuals
+            List<Particle> suggestedPopulation = new ArrayList<>();
+            JsonNode jsonSuggestedtPopulation = jsonTunerState.get("current_population");
+            for (JsonNode jsonSuggestedParticle: jsonSuggestedtPopulation ){
+                Particle suggestedParticle = new Particle();
+
+                // Todo: fill the individual
+                suggestedParticle.setCandidate();
+
+                float fitness = Float.valueOf(jsonSuggestedParticle.get("fitness").textValue());
+                suggestedParticle.setFitness(fitness);
+            }
+
+//            List<Particle> suggestedPopulation = jobTunerState.getCurrentPopulation();
+
+            for (Particle suggestedParticle: suggestedPopulation){
+
                 List<Float> candidate = suggestedParticle.getCandidate();
                 JobExecution jobExecution = new JobExecution();
                 JobSuggestedParamValue jobSuggestedParamValue = new JobSuggestedParamValue();
 
-                jobExecution.jobId = jobId;
-                jobExecution.algoId = job.algo.algoId;
+                jobExecution.job = job;
+                jobExecution.algo = job.algo;
                 jobExecution.isDefaultExecution = false;
+
                 for (int i=0; i< candidate.size() && i<paramList.size(); i++){
-                    jobSuggestedParamValue.paramId = paramList.get(i).paramId;
+                    jobSuggestedParamValue.algoParam.paramId = paramList.get(i).paramId;
                     jobSuggestedParamValue.paramValue = Float.toString(candidate.get(i));
                 }
+
                 if (isParamConstraintViolated(jobSuggestedParamValue)){
                     jobExecution.paramSetState = JobExecution.ParamSetStatus.FITNESS_COMPUTED;
                     jobExecution.resourceUsage = (double) -1;
@@ -70,12 +112,17 @@ public abstract class ParamGenerator {
                     jobExecution.paramSetState = JobExecution.ParamSetStatus.CREATED;
                 }
 
-                Long paramSetId = saveSuggestedParamMetadata(jobExecution);
-                jobSuggestedParamValue.paramSetId = paramSetId;
+                jobSuggestedParamValue.jobExecution = jobExecution;
+                suggestedParticle.setJobExecution(jobExecution);
+
+                jobExecutionList.add(jobExecution);
                 jobSuggestedParamValueList.add(jobSuggestedParamValue);
             }
-            saveSuggestedParams(jobSuggestedParamValueList);
         }
+
+        saveSuggestedParamMetadata(jobExecutionList);
+        saveSuggestedParams(jobSuggestedParamValueList);
+        saveTunerState(jobTunerStateList);
     }
 
     private boolean isParamConstraintViolated(JobSuggestedParamValue jobSuggestedParamValue){
@@ -91,7 +138,7 @@ public abstract class ParamGenerator {
 
     }
 
-    private Long saveSuggestedParamMetadata(JobExecution jobExecutionList){
+    private void saveSuggestedParamMetadata(List<JobExecution> jobExecutionList){
 
     }
 
