82d34d740e313811402342c9fb15a0567a6b17d2
Arpan Agrawal <aragrawal@linkedin.com>
2017-11-09 05:49:13 -0800

    Modified arguments to be sent to python

--- app/com/linkedin/drelephant/tunin/PSOParamGenerator.java
+++ app/com/linkedin/drelephant/tunin/PSOParamGenerator.java
@@ -1,26 +1,20 @@
 package com.linkedin.drelephant.tunin;
 
-import org.python.core.Py;
-import org.python.core.PyInstance;
-import org.python.core.PyString;
-import org.python.core.PySystemState;
-import org.python.util.PythonInterpreter;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import play.libs.Json;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
 
 public class PSOParamGenerator extends ParamGenerator{
 
     public TunerState generateParamSet(TunerState tunerState){
         JsonNode jsonTunerState = Json.toJson(tunerState);
-        String stringTunerState = jsonTunerState.toString();
+        String stringTunerState = jsonTunerState.get("_stringTunerState").toString();
+        String parametersToTune = jsonTunerState.get("_parametersToTune").toString();
         try{
-            Process p = Runtime.getRuntime().exec("/home/aragrawa/virtualenvs/auto-tuning/bin/python pso_param_generation.py " + stringTunerState);
+            Process p = Runtime.getRuntime().exec("/home/aragrawa/virtualenvs/auto-tuning/bin/python pso_param_generation.py " +stringTunerState+" "+parametersToTune);
             BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String updatedStringTunerState = in.readLine();
             tunerState.setStringTunerState(updatedStringTunerState);
