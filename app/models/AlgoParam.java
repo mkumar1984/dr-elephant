d8b829f0a1717aa5c2636ce021b6860492923d13
Arpan Agrawal <aragrawal@linkedin.com>
2017-11-10 04:34:00 -0800

    Composite id added

--- app/models/AlgoParam.java
+++ app/models/AlgoParam.java
@@ -1,20 +1,7 @@
 package models;
 
 import java.sql.Timestamp;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
+import javax.persistence.*;
 import play.db.ebean.Model;
 
 @Entity
--- app/models/JobSuggestedParamValue.java
+++ app/models/JobSuggestedParamValue.java
@@ -1,14 +1,15 @@
 package models;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
-
 import javax.persistence.*;
-
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import play.db.ebean.Model;
 
 
 @Entity
 @Table(name = "job_suggested_param_value")
+@IdClass(JobSuggestedParamValue.PrimaryKey.class)
 public class JobSuggestedParamValue extends Model{
 
   private static final long serialVersionUID = 1L;
@@ -21,6 +22,8 @@ public class JobSuggestedParamValue extends Model{
     public static final String updatedTs = "updatedTs";
   }
 
+  @EmbeddedId
+  public PrimaryKey paramValuePK;
   public String paramValue;
   public Timestamp createdTs;
   public Timestamp updatedTs;
@@ -33,5 +36,33 @@ public class JobSuggestedParamValue extends Model{
   @JoinTable(name="algo_param", joinColumns={@JoinColumn(name ="param_id", referencedColumnName="param_id")})
   public AlgoParam algoParam;
 
-  public static Finder<Integer, JobSuggestedParamValue> find = new Finder<Integer, JobSuggestedParamValue>(Integer.class, JobSuggestedParamValue.class);
+  @Embeddable
+  public static class PrimaryKey implements Serializable{
+      public Integer paramId;
+      public Integer paramSetId;
+
+      @Override
+      public int hashCode(){
+          return new HashCodeBuilder(17,37).append(paramId).append(paramSetId).toHashCode();
+      }
+
+      @Override
+      public boolean equals(Object obj){
+          if(obj instanceof PrimaryKey){
+              if(((PrimaryKey)obj).paramId == paramId && ((PrimaryKey)obj).paramSetId == paramSetId){
+                  return true;
+                }
+                else{
+                    return false;
+                }
+          } else{
+                return false;
+          }
+      }
+  }
+
+
+  public static Finder<PrimaryKey, JobSuggestedParamValue> find = new Finder<PrimaryKey, JobSuggestedParamValue>(PrimaryKey.class, JobSuggestedParamValue.class);
+
+
 }
