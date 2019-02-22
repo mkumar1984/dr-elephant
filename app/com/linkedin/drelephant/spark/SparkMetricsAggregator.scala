6eda008018177a06c286b52cea28086ea860212c
Manoj Kumar(Bangalore Engineering) <mkumar1@linkedin.com>
2019-02-22 18:25:08 +0530

    Removing spark total delay chanegs, adding memory overhead for resource usage calculation

--- app/com/linkedin/drelephant/spark/SparkMetricsAggregator.scala
+++ app/com/linkedin/drelephant/spark/SparkMetricsAggregator.scala
@@ -30,6 +30,9 @@ import com.linkedin.drelephant.spark.fetchers.statusapiv1.StageData
 import java.util.Date
 import com.linkedin.drelephant.AutoTuner
 import com.linkedin.drelephant.spark.fetchers.statusapiv1.StageStatus
+import com.linkedin.drelephant.ElephantContext
+import org.apache.hadoop.yarn.conf.YarnConfiguration
+import org.apache.hadoop.conf.Configuration
 
 class SparkMetricsAggregator(private val aggregatorConfigurationData: AggregatorConfigurationData)
     extends HadoopMetricsAggregator {
@@ -58,7 +61,7 @@ class SparkMetricsAggregator(private val aggregatorConfigurationData: Aggregator
     if (applicationDurationMillis < 0) {
       logger.warn(s"applicationDurationMillis is negative. Skipping Metrics Aggregation:${applicationDurationMillis}")
     } else {
-      var (resourcesActuallyUsed, resourcesAllocatedForUse) = calculateResourceUsage(data.executorSummaries, executorMemoryBytes)
+      var (resourcesActuallyUsed, resourcesAllocatedForUse) = calculateResourceUsage(data)
       val resourcesActuallyUsedWithBuffer = resourcesActuallyUsed.doubleValue() * (1.0 + allocatedMemoryWasteBufferPercentage)
       val resourcesWastedMBSeconds = (resourcesActuallyUsedWithBuffer < resourcesAllocatedForUse.doubleValue()) match {
         case true => resourcesAllocatedForUse.doubleValue() - resourcesActuallyUsedWithBuffer
@@ -77,24 +80,26 @@ class SparkMetricsAggregator(private val aggregatorConfigurationData: Aggregator
         logger.warn(s"allocatedMemoryWasteBufferPercentage:${allocatedMemoryWasteBufferPercentage}")
       }
       hadoopAggregatedData.setResourceWasted(resourcesWastedMBSeconds.toLong)
-      hadoopAggregatedData.setTotalDelay(computeTotalWaitingTime(data.stageDatas))
     }
   }
 
   //calculates the resource usage by summing up the resources used per executor
-  private def calculateResourceUsage(executorSummaries: Seq[ExecutorSummary], executorMemoryBytes: Long): (BigInt, BigInt) = {
+  private def calculateResourceUsage(data: SparkApplicationData): (BigInt, BigInt) = {
+    val executorSummaries = data.executorSummaries
     var sumResourceUsage: BigInt = 0
     var sumResourcesAllocatedForUse: BigInt = 0
     executorSummaries.foreach(
       executorSummary => {
-        var memUsedBytes: Long = executorSummary.peakJvmUsedMemory.getOrElse(JVM_USED_MEMORY, 0).asInstanceOf[Number].longValue + MemoryFormatUtils.stringToBytes(SPARK_RESERVED_MEMORY)
+        val memoryOverhead = overheadMemoryBytesOf(data).get
+        val roundedContainerBytes = getRoundedContainerBytes(data).get
+        var memUsedBytes: Long = executorSummary.peakJvmUsedMemory.getOrElse(JVM_USED_MEMORY, 0).asInstanceOf[Number].longValue + MemoryFormatUtils.stringToBytes(SPARK_RESERVED_MEMORY) + memoryOverhead
         var timeSpent: Long = executorSummary.totalDuration
         var totalCores: Int = executorSummary.totalCores
-        if(totalCores==0){
-          totalCores=1;
+        if (totalCores == 0) {
+          totalCores = 1;
         }
-        val bytesMillisUsed = BigInt(memUsedBytes) * timeSpent/totalCores
-        val bytesMillisAllocated = BigInt(executorMemoryBytes) * timeSpent/totalCores
+        val bytesMillisUsed = BigInt(memUsedBytes) * timeSpent / totalCores
+        val bytesMillisAllocated = BigInt(roundedContainerBytes) * timeSpent / totalCores
         sumResourcesAllocatedForUse += (bytesMillisAllocated / (BigInt(FileUtils.ONE_MB) * BigInt(Statistics.SECOND_IN_MS)))
         sumResourceUsage += (bytesMillisUsed / (BigInt(FileUtils.ONE_MB) * BigInt(Statistics.SECOND_IN_MS)))
       })
@@ -128,119 +133,44 @@ class SparkMetricsAggregator(private val aggregatorConfigurationData: Aggregator
   private def totalExecutorTaskTimeMillisOf(data: SparkApplicationData): BigInt = {
     data.executorSummaries.map { executorSummary => BigInt(executorSummary.totalDuration) }.sum
   }
-
-  /**
-   * This method computes total waiting time by looking at each stage's waiting time and using dependencies between
-   * each stage. Total waiting time is the difference between actual duration of the job and duration of the job
-   * if there was no scheduler delay.
-   */
-  def computeTotalWaitingTime(stageDatas: Seq[StageData]) = {
-    val notSkippedStages = for (stage <- stageDatas if !stage.status.equals(StageStatus.SKIPPED)) yield stage
-    val dependencyMap = getStageDependency(notSkippedStages);
-
-    val sortedStages = notSkippedStages.sortBy { _.submissionTime }
-    var completionTimeWithoutDelayMap: scala.collection.mutable.Map[Int, Long] = scala.collection.mutable.Map[Int, Long]()
-    var completionTimeWithDelayMap: scala.collection.mutable.Map[Int, Long] = scala.collection.mutable.Map[Int, Long]()
-    var finalStageCompletionTimeWithoutDelay = 0L;
-    var finalStageActualCompletionTime = 0L;
-
-    for (i <- 0 until sortedStages.length) {
-      val stage = sortedStages(i)
-      val stageId = stage.stageId
-      var maxCompletionTimeWithoutDelay: Long = 0L;
-      var maxCompletionWithDelay: Long = 0L;
-
-      for (dependeeStageId <- dependencyMap.get(stageId).get) {
-        maxCompletionTimeWithoutDelay = Math.max(maxCompletionTimeWithoutDelay, completionTimeWithoutDelayMap.getOrElse(dependeeStageId, 0L));
-        maxCompletionWithDelay = Math.max(maxCompletionWithDelay, completionTimeWithDelayMap.getOrElse(dependeeStageId, 0L));
-      }
-      if (maxCompletionTimeWithoutDelay == 0) {
-        maxCompletionTimeWithoutDelay = stage.submissionTime.get.getTime;
-        maxCompletionWithDelay = stage.submissionTime.get.getTime
-      }
-      val currentStageDelay = getStageDelay(stage)
-      val completionTimeWithoutDelay = maxCompletionTimeWithoutDelay + (stage.completionTime.get.getTime - stage.submissionTime.get.getTime) - currentStageDelay + (stage.submissionTime.get.getTime - maxCompletionWithDelay)
-
-      logger.debug("currentStageDelay for " + stageId + ":" + currentStageDelay)
-      logger.debug("Delay till this stage " + stageId + ":" + (stage.completionTime.get.getTime - completionTimeWithoutDelay))
-
-      completionTimeWithoutDelayMap.put(stageId, completionTimeWithoutDelay)
-      completionTimeWithDelayMap.put(stageId, stage.completionTime.get.getTime)
-
-      finalStageCompletionTimeWithoutDelay = Math.max(finalStageCompletionTimeWithoutDelay, completionTimeWithoutDelay);
-      finalStageActualCompletionTime = Math.max(finalStageActualCompletionTime, stage.completionTime.get.getTime)
+  private def overheadMemoryBytesOf(data: SparkApplicationData): Option[Long] = {
+    val executorMemory = executorMemoryBytesOf(data)
+    val appConfigurationProperties = data.appConfigurationProperties
+    var memoryOverhead = None: Option[Long]
+    if (appConfigurationProperties.get(SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD).isEmpty) {
+      val overheadMemory = executorMemory.get * (appConfigurationProperties.get(SPARK_MEMORY_OVERHEAD_MULTIPLIER_PERCENT).getOrElse(DEFAULT_SPARK_MEMORY_OVERHEAD_MULTIPLIER_PERCENT)).toInt / 100
+      Option(overheadMemory)
+    } else {
+      appConfigurationProperties.get(SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD).map(MemoryFormatUtils.stringToBytes)
     }
-
-    logger.debug("Total Delay:" + (finalStageActualCompletionTime - finalStageCompletionTimeWithoutDelay))
-    finalStageActualCompletionTime - finalStageCompletionTimeWithoutDelay
   }
 
-  /**
-   * Returns scheduler delay for the job, which is max of scheduler delay distribution
-   */
-  def getStageDelay(stage: StageData): Long = {
-    val maxSchedulerDelayIndex = 4
-    var schedulerDelay = 0D
-    val taskSummary = stage.taskSummary.getOrElse(null)
-    if (taskSummary != null) {
-      val schedulerDelays = taskSummary.schedulerDelay
-      if (schedulerDelays != null)
-        schedulerDelay = schedulerDelays(maxSchedulerDelayIndex)
-    }
-    schedulerDelay.longValue()
+  private def getRoundedContainerBytes(data: SparkApplicationData): Option[Long] = {
+    val increment = getIncrementBytes()
+    val executorMemory = executorMemoryBytesOf(data)
+    val memoryOverHead = overheadMemoryBytesOf(data)
+    val totalMemoryRequired = executorMemory.get + memoryOverHead.get
+    val roundedContainerBytes = Math.ceil((totalMemoryRequired * 1.0) / increment.get) * increment.get
+    Option(roundedContainerBytes.longValue())
   }
 
-  /**
-   * Get approximate dependency between each stages, using submission time and completion time
-   * A stage is dependent on other stage if it is started within 1 second of any stage's completion.
-   */
-  def getStageDependency(stageDatas: Seq[StageData]): scala.collection.mutable.Map[Int, scala.collection.mutable.Set[Int]] = {
-    var dependencyMap: scala.collection.mutable.Map[Int, scala.collection.mutable.Set[Int]] = scala.collection.mutable.Map[Int, scala.collection.mutable.Set[Int]]();
-    for (i <- 0 until stageDatas.length) {
-      var dependentStages = findDependeeStages(i, stageDatas);
-      dependencyMap.put(stageDatas(i).stageId, dependentStages)
-    }
-    logger.debug("Depdendency Map " + dependencyMap)
-    dependencyMap
-  }
-
-  /**
-   * Find dependee stages for the givem stage
-   */
-  def findDependeeStages(dependeeStageIndex: Int, stages: Seq[StageData]): scala.collection.mutable.Set[Int] = {
-    val dependentStage = stages(dependeeStageIndex)
-    val dependentStageStartTime: Date = dependentStage.submissionTime.getOrElse(null)
-    var dependeeStages: scala.collection.mutable.Set[Int] = scala.collection.mutable.Set[Int]()
-    var timeBasedDependentStage = -1
-    var timeBasedDependetStageCompletionTime = new Date(0);
-
-    for (j <- 0 until stages.length if dependeeStageIndex != j) {
-      val possibleDependeeStage = stages(j)
-      val possiblDependeeStageCompletionTime: Date = possibleDependeeStage.completionTime.getOrElse(null)
-      logger.debug("Current dependent Stage " + j + " date " + possiblDependeeStageCompletionTime);
-      if (dependentStageStartTime.before(possiblDependeeStageCompletionTime) == false) {
-        if (possiblDependeeStageCompletionTime.after(timeBasedDependetStageCompletionTime)) {
-          timeBasedDependentStage = j;
-          timeBasedDependetStageCompletionTime = possiblDependeeStageCompletionTime;
-        }
-        if (dependentStageStartTime.getTime <= possiblDependeeStageCompletionTime.getTime + AutoTuner.ONE_SEC) {
-          dependeeStages.add(possibleDependeeStage.stageId)
-        }
-      }
-    }
-    if (dependeeStages.size == 0 && timeBasedDependentStage != -1) {
-      dependeeStages.add(timeBasedDependentStage)
-    }
-    dependeeStages
+  private def getIncrementBytes(): Option[Long] = {
+    val config = new Configuration
+    val incrementMB = config.getInt(YarnConfiguration.RM_SCHEDULER_MINIMUM_ALLOCATION_MB,
+      YarnConfiguration.DEFAULT_RM_SCHEDULER_MINIMUM_ALLOCATION_MB);
+    Option(incrementMB * 1024 * 1024)
   }
 }
 
 object SparkMetricsAggregator {
   /** The percentage of allocated memory we expect to waste because of overhead. */
+  val DEFAULT_SPARK_MEMORY_OVERHEAD_MULTIPLIER_PERCENT = "10"
   val DEFAULT_ALLOCATED_MEMORY_WASTE_BUFFER_PERCENTAGE = 0.5D
   val ALLOCATED_MEMORY_WASTE_BUFFER_PERCENTAGE_KEY = "allocated_memory_waste_buffer_percentage"
   val SPARK_RESERVED_MEMORY: String = "300M"
   val SPARK_EXECUTOR_INSTANCES_KEY = "spark.executor.instances"
   val SPARK_EXECUTOR_MEMORY_KEY = "spark.executor.memory"
+  val SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD = "spark.yarn.executor.memoryOverhead"
+  val SPARK_MEMORY_OVERHEAD_MULTIPLIER_PERCENT = "spark.memoryOverhead.multiplier.percent"
   val JVM_USED_MEMORY = "jvmUsedMemory"
 }
--- test/com/linkedin/drelephant/spark/SparkMetricsAggregatorTest.scala
+++ test/com/linkedin/drelephant/spark/SparkMetricsAggregatorTest.scala
@@ -56,58 +56,16 @@ class SparkMetricsAggregatorTest extends FunSpec with Matchers {
     }
 
     val executorSummaries = Seq(
-      newFakeExecutorSummary(id = "1", totalDuration = 1000000L, Map("jvmUsedMemory" -> 394567123)),
-      newFakeExecutorSummary(id = "2", totalDuration = 3000000L, Map("jvmUsedMemory" -> 23456834))
+      newFakeExecutorSummary(id = "1", totalDuration = 1000000L, totalCores=3,  Map("jvmUsedMemory" -> 394567123)),
+      newFakeExecutorSummary(id = "2", totalDuration = 3000000L, totalCores=3,  Map("jvmUsedMemory" -> 23456834))
 
     )
-
-    val cal = Calendar.getInstance()
-    cal.setTimeInMillis(1550340951000L)
-    val stage1StartTime = cal.getTime
-    val stage1CompletionTime = DateUtils.addMinutes(stage1StartTime, 3)
-
-    val stage2StartTime = DateUtils.addMilliseconds(DateUtils.addMinutes(stage1StartTime, 3), 1)
-    val stage2CompletionTime = DateUtils.addMinutes(stage1StartTime, 9)
-
-    val stage3StartTime = DateUtils.addMilliseconds(DateUtils.addMinutes(stage1StartTime, 9), 999)
-    val stage3CompletionTime = DateUtils.addMinutes(stage1StartTime, 10)
-
-    val stage4StartTime = DateUtils.addMilliseconds(DateUtils.addMinutes(stage1StartTime, 10), 1)
-    val stage4CompletionTime = DateUtils.addMinutes(stage1StartTime, 17)
-
-    val stage5StartTime = DateUtils.addMilliseconds(DateUtils.addMinutes(stage1StartTime, 17), 1)
-    val stage5CompletionTime = DateUtils.addMinutes(stage1StartTime, 18)
-
-    val stage6StartTime = DateUtils.addMilliseconds(DateUtils.addMinutes(stage1StartTime, 18), 1)
-    val stage6CompletionTime = DateUtils.addMinutes(stage1StartTime, 30)
-
-    val scheduleStage1Delay = IndexedSeq(0D, 0D, 0D, 0D, AutoTuner.ONE_MIN * 1);
-    val scheduleStage2Delay = IndexedSeq(0D, 0D, 0D, 0D, AutoTuner.ONE_MIN * 0.7);
-    val scheduleStage3Delay = IndexedSeq(0D, 0D, 0D, 0D, 0D);
-    val scheduleStage4Delay = IndexedSeq(0D, 0D, 0D, 0D, AutoTuner.ONE_MIN * 3.0);
-    val scheduleStage5Delay = IndexedSeq(0D, 0D, 0D, 0D, AutoTuner.ONE_MIN * 1.0);
-    val scheduleStage6Delay = IndexedSeq(0D, 0D, 0D, 0D, AutoTuner.ONE_MIN * 0D);
-
-    val taskSummary1 = new TaskMetricDistributionsImpl(null, null, null, null, null, null, null, null, null, scheduleStage1Delay, null, null, null, null, null, null, null);
-    val taskSummary2 = new TaskMetricDistributionsImpl(null, null, null, null, null, null, null, null, null, scheduleStage2Delay, null, null, null, null, null, null, null);
-    val taskSummary3 = new TaskMetricDistributionsImpl(null, null, null, null, null, null, null, null, null, scheduleStage3Delay, null, null, null, null, null, null, null);
-    val taskSummary4 = new TaskMetricDistributionsImpl(null, null, null, null, null, null, null, null, null, scheduleStage4Delay, null, null, null, null, null, null, null);
-    val taskSummary5 = new TaskMetricDistributionsImpl(null, null, null, null, null, null, null, null, null, scheduleStage5Delay, null, null, null, null, null, null, null);
-    val taskSummary6 = new TaskMetricDistributionsImpl(null, null, null, null, null, null, null, null, null, scheduleStage6Delay, null, null, null, null, null, null, null);
-    val stageDatas = Seq(
-      newFakeStageData(StageStatus.COMPLETE, 0, 10, Option(stage1StartTime), Option(stage1CompletionTime), "stage1", 1000990929L, 100000L, 2000990929L, 200000L, 4000990929L, 600000L, 24570990929L, 245990929L, 34570990929L, 34570990929L, Option(taskSummary1)),
-      newFakeStageData(StageStatus.SKIPPED, 1, 11, Option(stage2StartTime), Option(stage2CompletionTime), "stage2", 2000990929L, 200000L, 3000990929L, 300000L, 5000990929L, 700000L, 23570990929L, 345990929L, 44570990929L, 44570990929L, Option(taskSummary2)),
-      newFakeStageData(StageStatus.COMPLETE, 2, 12, Option(stage3StartTime), Option(stage3CompletionTime), "stage3", 3000990929L, 300000L, 4000990929L, 400000L, 6000990929L, 800000L, 22570990929L, 445990929L, 54570990929L, 4570990929L, Option(taskSummary3)),
-      newFakeStageData(StageStatus.COMPLETE, 3, 13, Option(stage4StartTime), Option(stage4CompletionTime), "stage4", 4000990929L, 400000L, 5000990929L, 500000L, 7000990929L, 900000L, 21570990929L, 545990929L, 64570990929L, 64570990929L, Option(taskSummary4)),
-      newFakeStageData(StageStatus.COMPLETE, 4, 14, Option(stage5StartTime), Option(stage5CompletionTime), "stage5", 5000990929L, 500000L, 6000990929L, 600000L, 8000990929L, 2000000L, 20570990929L, 645990929L, 74570990929L, 4570990929L, Option(taskSummary5)),
-      newFakeStageData(StageStatus.COMPLETE, 5, 15, Option(stage6StartTime), Option(stage6CompletionTime), "stage6", 6000990929L, 600000L, 7000990929L, 700000L, 9000990929L, 2100000L, 27570990929L, 845990929L, 84570990929L, 84570990929L, Option(taskSummary6)))
-
 
     val restDerivedData = {
       SparkRestDerivedData(
         applicationInfo,
         jobDatas = Seq.empty,
-        stageDatas = stageDatas,
+        stageDatas = Seq.empty,
         executorSummaries = executorSummaries,
         stagesWithFailedTasks = Seq.empty
       )
@@ -122,7 +80,8 @@ class SparkMetricsAggregatorTest extends FunSpec with Matchers {
             "spark.driver.memory" -> "2G",
             "spark.executor.instances" -> "2",
             "spark.executor.memory" -> "4g",
-            "spark.shuffle.memoryFraction" -> "0.5"
+            "spark.shuffle.memoryFraction" -> "0.5",
+            "spark.memoryOverhead.multiplier.percent" -> "15"
           )
         )
         SparkLogDerivedData(environmentUpdate)
@@ -136,18 +95,20 @@ class SparkMetricsAggregatorTest extends FunSpec with Matchers {
       val result = aggregator.getResult
 
       it("calculates resources used (allocated)") {
-        result.getResourceUsed should be(4096000+12288000)
+        result.getResourceUsed should be(6826666)
       }
 
       it("calculates resources wasted") {
-        val resourceAllocated = 4096000+12288000
-        val resourceUsed = 676288+967110
-        result.getResourceWasted should be(resourceAllocated.toDouble - resourceUsed.toDouble * 1.5)
+        val resourceAllocated = 6826666
+        val resourceUsed = 1366999
+        val resourceWasted: Long = (resourceAllocated.toDouble - resourceUsed.toDouble * 1.5).longValue()
+        result.getResourceWasted should be(resourceWasted)
       }
 
-      it("calculate total delay") {
-        result.getTotalDelay should be(300000L)
+      it("doesn't calculate total delay") {
+        result.getTotalDelay should be(0L)
       }
+
       it("sets resource used as 0 when duration is negative") {
         //make the duration negative
         val applicationInfo = {
@@ -223,6 +184,7 @@ object SparkMetricsAggregatorTest {
   def newFakeExecutorSummary(
     id: String,
     totalDuration: Long,
+    totalCores: Int,
     peakJvmUsedMemory: Map[String, Long]
   ): ExecutorSummaryImpl = new ExecutorSummaryImpl(
     id,
@@ -230,7 +192,7 @@ object SparkMetricsAggregatorTest {
     rddBlocks = 0,
     memoryUsed = 0,
     diskUsed = 0,
-    totalCores = 1,
+    totalCores = totalCores,
     activeTasks = 0,
     failedTasks = 0,
     completedTasks = 0,
@@ -247,58 +209,4 @@ object SparkMetricsAggregatorTest {
     peakJvmUsedMemory,
     peakUnifiedMemory = Map.empty
   )
-
-  def newFakeStageData(
-    status: StageStatus,
-    stageId: Int,
-    numCompleteTasks: Int,
-    submissionTime: Option[Date],
-    completionTime: Option[Date],
-    name: String,
-    inputBytes: Long,
-    inputRecords: Long,
-    outputBytes: Long,
-    outputRecords: Long,
-    shuffleReadBytes: Long,
-    shuffleReadRecords: Long,
-    shuffleWriteBytes: Long,
-    shuffleWriteRecords: Long,
-    memoryBytesSpilled: Long,
-    diskBytesSpilled: Long,
-    taskSummary: Option[TaskMetricDistributionsImpl]): StageDataImpl = new StageDataImpl(
-    status,
-    stageId,
-    attemptId = 0,
-    numTasks = numCompleteTasks + 0,
-    numActiveTasks = numCompleteTasks + 0,
-    numCompleteTasks,
-    numFailedTasks = 0,
-    executorRunTime = 0L,
-    executorCpuTime = 0,
-    submissionTime,
-    firstTaskLaunchedTime = None,
-    completionTime,
-    failureReason = None,
-    inputBytes,
-    inputRecords,
-    outputBytes,
-    outputRecords,
-    shuffleReadBytes,
-    shuffleReadRecords,
-    shuffleWriteBytes,
-    shuffleWriteRecords,
-    memoryBytesSpilled,
-    diskBytesSpilled,
-    name,
-    details = "",
-    schedulingPool = "",
-    accumulatorUpdates = Seq.empty,
-    tasks = None,
-    executorSummary = None,
-    peakJvmUsedMemory = None,
-    peakExecutionMemory = None,
-    peakStorageMemory = None,
-    peakUnifiedMemory = None,
-    taskSummary,
-    executorMetricsSummary = None)
 }
