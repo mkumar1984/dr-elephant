4c30396b018f3c0da682063dd3f8265a774f1b72
Manoj Kumar(Bangalore Engineering) <mkumar1@linkedin.com>
2019-08-30 17:57:58 +0530

    LIHADOOP-48670: Fixing Resource Usage Computation for Spark

--- app/com/linkedin/drelephant/spark/legacydata/LegacyDataConverters.scala
+++ app/com/linkedin/drelephant/spark/legacydata/LegacyDataConverters.scala
@@ -233,6 +233,8 @@ object LegacyDataConverters {
         executorInfo.shuffleRead,
         executorInfo.shuffleWrite,
         executorInfo.maxMem,
+        new Date(executorInfo.addTime),
+        if(executorInfo.removeTime!=null) new Date(executorInfo.removeTime) else null,
         executorInfo.totalGCTime,
         executorInfo.totalMemoryBytesSpilled,
         executorLogs = Map.empty,
